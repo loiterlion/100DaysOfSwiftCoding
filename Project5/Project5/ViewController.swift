c55d2279e8581f0b736bd82dbeb65a41fdfa17cd
loiterlion <brucewang7777@gmail.com>
2024-09-28 21:07:02 +0800

    fix a bug

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -64,43 +64,81 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    fileprivate func showError(_ errorTitle: String, _ errorMsg: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMsg, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     func submit(_ answer: String) {
         let lowercased = answer.lowercased()
         
         var errorTitle: String = ""
         var errorMsg: String = ""
         
-        if isPossible(lowercased) {
-            if isOriginal(lowercased) {
-                errorTitle = "Word used already"
-                errorMsg = "Be more original!"
-                if isReal(lowercased) {
-                    usedWords.insert(answer, at: 0)
-                    tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
-                    return
-                } else {
-                    errorTitle = "Word not recognised"
-                    errorMsg = "You can't just make them up, you know!"
-
-                }
-            } else {
-                errorTitle = "Word used already"
-                errorMsg = "Be more original!"
-            }
-        } else {
+        
+        if !isPossible(lowercased) {
             errorTitle = "Not possible!"
             errorMsg = "You can't spell the that from the word \(lowercased)"
+            showError(errorTitle, errorMsg)
+            return
         }
-    
-        let ac = UIAlertController(title: errorTitle, message: errorMsg, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        
+        if !isOriginal(lowercased) {
+            errorTitle = "Word used already"
+            errorMsg = "Be more original!"
+            showError(errorTitle, errorMsg)
+            return
+        }
+        
+        if !isReal(lowercased) {
+            errorTitle = "Word not recognised"
+            errorMsg = "You can't just make them up, you know!"
+            showError(errorTitle, errorMsg)
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
     }
+    
+//    func submit(_ answer: String) {
+//        let lowercased = answer.lowercased()
+//
+//        var errorTitle: String = ""
+//        var errorMsg: String = ""
+//
+//        if isPossible(lowercased) {
+//            if isOriginal(lowercased) {
+//                errorTitle = "Word used already"
+//                errorMsg = "Be more original!"
+//                if isReal(lowercased) {
+//                    usedWords.insert(answer, at: 0)
+//                    tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+//                    return
+//                } else {
+//                    errorTitle = "Word not recognised"
+//                    errorMsg = "You can't just make them up, you know!"
+//
+//                }
+//            } else {
+//                errorTitle = "Word used already"
+//                errorMsg = "Be more original!"
+//            }
+//        } else {
+//            errorTitle = "Not possible!"
+//            errorMsg = "You can't spell the that from the word \(lowercased)"
+//        }
+//
+//        let ac = UIAlertController(title: errorTitle, message: errorMsg, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//        present(ac, animated: true)
+//    }
     
     func isPossible(_ word: String) -> Bool {
         guard var tmp = title?.lowercased() else { return false}
         
-        for letter in tmp {
+        for letter in word {
             if let index = tmp.firstIndex(of: letter) {
                 tmp.remove(at: index)
             } else {
