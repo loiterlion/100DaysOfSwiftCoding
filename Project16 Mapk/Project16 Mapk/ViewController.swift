367ab5b6b54386040f5bd86c65fabb64de48d13c
loiterlion <brucewang7777@gmail.com>
2024-11-01 10:18:41 +0800

    Day 61 Wrapup Task 1
    Try typecasting the return value from dequeueReusableAnnotationView() so that it's an MKPinAnnotationView. Once that’s done, change the pinTintColor property to your favorite UIColor.

--- Project16 Mapk/Project16 Mapk/ViewController.swift
+++ Project16 Mapk/Project16 Mapk/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         let identifier = "Capital"
         
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
         
         if annotationView == nil {
             annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
@@ -39,6 +39,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
         } else {
             annotationView?.annotation = annotation
         }
+        
+        annotationView?.markerTintColor = .blue
+        
         return annotationView
     }
     
