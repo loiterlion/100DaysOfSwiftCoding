4eaa37646b8eb82961fcb76ce7745a4960134671
loiterlion <brucewang7777@gmail.com>
2024-11-01 15:47:29 +0800

    Day 61 Wrapup Task 2

--- Project16 Mapk/Project16 Mapk/ViewController.swift
+++ Project16 Mapk/Project16 Mapk/ViewController.swift
@@ -11,16 +11,43 @@ import UIKit
 class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    fileprivate func addAnnotations() {
         // Do any additional setup after loading the view.
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
-
+        
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+    }
+    
+    func mapTypeChoosed(alertAction: UIAlertAction) {
+        guard let title = alertAction.title else { return }
+        let array = ["standard", "satellite", "hybrid", "satelliteFlyover", "hybridFlyover", "mutedStandard"]
+        if let index = array.firstIndex(of: title) {
+            mapView.mapType = MKMapType.init(rawValue: UInt(index)) ?? .standard
+            navigationItem.rightBarButtonItem?.title = title
+        }
+    }
+    
+    @objc func chooseMapType() {
+        
+        let ac = UIAlertController(title: "Choose a map type", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "standard", style: .default, handler: mapTypeChoosed))
+        ac.addAction(UIAlertAction(title: "satellite", style: .default, handler: mapTypeChoosed))
+        ac.addAction(UIAlertAction(title: "hybrid", style: .default, handler: mapTypeChoosed))
+        ac.addAction(UIAlertAction(title: "satelliteFlyover", style: .default, handler: mapTypeChoosed))
+        ac.addAction(UIAlertAction(title: "mutedStandard", style: .default, handler: mapTypeChoosed))
+        
+        present(ac, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Map Type", style: .plain, target: self, action: #selector(chooseMapType))
+        addAnnotations()
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
