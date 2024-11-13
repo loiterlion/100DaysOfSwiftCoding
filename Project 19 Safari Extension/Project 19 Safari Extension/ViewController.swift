0d4c310a88f1eeac7cffc59d7e637ecf6527b56f
loiterlion <brucewang7777@gmail.com>
2024-11-13 10:18:02 +0800

    Project 19 Safari Extension

---
+++ Project 19 Safari Extension/Project 19 Safari Extension/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 19 Safari Extension
+//
+//  Created by Bruce on 2024/11/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
