4155b4e589dac4c4ee8e4c89b6c9fb695b6bb32c
loiterlion <brucewang7777@gmail.com>
2024-11-13 10:17:38 +0800

    Project 22 iBeacon

---
+++ Project 22 iBeacon/Project 22 iBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 22 iBeacon
+//
+//  Created by Bruce on 2024/11/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let image = UIImage(named: "test")
+    }
+
+
+}
+
