129caedfadfef719123a93a57330cf7562d13e78
loiterlion <brucewang7777@gmail.com>
2025-02-25 21:49:08 +0800

    Homework for Project 26

--- Project 26 Core Motion/Project 26 Core Motion/GameScene.swift
+++ Project 26 Core Motion/Project 26 Core Motion/GameScene.swift
@@ -13,7 +13,8 @@ enum CollisionTypes: UInt32 {
     case wall = 2
     case star = 4
     case vortex = 8
-    case finish = 16
+    case teleport = 16
+    case finish = 32
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -31,20 +32,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    var totalLevelCount = 3
+    var nextLevel = 1
+    
+    var teleports = [SKSpriteNode]()
+    var currentTeleport: SKSpriteNode?
+    
     override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "background")
-        background.position = CGPoint(x: 512, y: 384)
-        background.blendMode = .replace
-        background.zPosition = -1
-        addChild(background)
-        
-        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.text = "Score: 0"
-        scoreLabel.horizontalAlignmentMode = .left
-        scoreLabel.position = CGPoint(x: 16, y: 16)
-        scoreLabel.zPosition = 2
-        addChild(scoreLabel)
-        
         loadLevel()
         createPlayer()
         physicsWorld.gravity = CGVector.zero
@@ -106,13 +100,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(node)
     }
     
+    func loadTeleport(_ position: CGPoint) {
+        let node = SKSpriteNode(imageNamed: "teleport")
+        node.position = position
+        node.name = "teleport"
+        node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        node.physicsBody?.isDynamic = false
+        node.physicsBody?.categoryBitMask = CollisionTypes.teleport.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        
+        addChild(node)
+        
+        teleports.append(node)
+    }
+    
     func loadLevel() {
-        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+        let nextLevelFilename = "level\(nextLevel)"
+        guard let levelURL = Bundle.main.url(forResource: nextLevelFilename, withExtension: "txt") else {
             fatalError("Cound not find level1.txt in the app bundle")
         }
         guard let levelString = try? String(contentsOf: levelURL) else {
             fatalError("Cound not load level1.txt in the app bundle")
         }
+        
+        // add background
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        // add scorelabel
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
         
         let lines = levelString.components(separatedBy: "\n")
         for (row, line) in lines.reversed().enumerated() {
@@ -127,6 +152,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     loadStart(position)
                 } else if letter == "f" {
                     loadFinishPoint(position)
+                } else if letter == "t" {
+                    loadTeleport(position)
                 } else if letter == " " {
                     
                 } else {
@@ -136,16 +163,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
 
-    func createPlayer() {
+    func createPlayer(_ position: CGPoint = CGPoint(x: 96, y: 672)) {
         player = SKSpriteNode(imageNamed: "player")
-        player.position = CGPoint(x: 96, y: 672)
+        player.position = position
         player.zPosition = 1
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
         player.physicsBody?.allowsRotation = false
         player.physicsBody?.linearDamping = 0.5
         
         player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
-        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue | CollisionTypes.teleport.rawValue
         player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
         
         addChild(player)
@@ -169,6 +196,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         guard isGameOver == false else { return }
+        
+        if let tmp = currentTeleport {
+            if !player.intersects(tmp) {
+                currentTeleport = nil
+            }
+        }
         
         #if targetEnvironment(simulator)
         if let lastTouchPosition = lastTouchPosition {
@@ -196,6 +229,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func moveToNextTeleport(_ node: SKNode) {
+        teleports.enumerated().forEach { (index, tmp) in
+            if tmp == node {
+                let cnt = teleports.count
+                let nextNode = teleports[(index + 1) % cnt]
+                player.removeFromParent()
+                createPlayer(nextNode.position)
+                currentTeleport = nextNode
+            }
+        }
+    }
+    
     func playerCollided(with node: SKNode) {
         if node.name == "vortex" {
             player.physicsBody?.isDynamic = false
@@ -214,16 +259,67 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         } else if node.name == "star" {
             node.removeFromParent()
             score += 1
+        } else if node.name == "teleport" {
+            // move to next teleport
+            if currentTeleport == nil {
+                moveToNextTeleport(node)
+            }
         } else if node.name == "finish" {
-            player.removeFromParent()
             
-            let finishLabel = SKLabelNode(fontNamed: "Chalkduster")
-            finishLabel.name = ""
-            finishLabel.text = "游戏结束：得分：\(score)"
-            finishLabel.horizontalAlignmentMode = .left
-            finishLabel.position = CGPoint(x: 512, y: 384)
-            finishLabel.zPosition = 2
-            addChild(finishLabel)
+            showLevelCompleteAlert()
+        }
+    }
+    
+    func showLevelCompleteAlert() {
+        let ac = UIAlertController(title: "Next Level", message: "Replay current level or continue to the next", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Replay", style: .default, handler: { [self] ac in
+            player.removeFromParent()
+            self.removeAllChildren()
+            self.loadLevel()
+            self.createPlayer()
+        }))
+        ac.addAction(UIAlertAction(title: "Next Level", style: .default, handler: { [self] ac in
+            self.nextLevel += 1
+            if nextLevel > totalLevelCount {
+                showGameCompleteAlert()
+            } else {
+                player.removeFromParent()
+                self.removeAllChildren()
+                self.loadLevel()
+                self.createPlayer()
+            }
+        }))
+        
+        // Present the alert on the root view controller
+        if let viewController = self.view?.window?.rootViewController {
+            viewController.present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    func showGameCompleteAlert() {
+//        let finishLabel = SKLabelNode(fontNamed: "Chalkduster")
+//        finishLabel.name = ""
+//        finishLabel.text = "游戏结束：得分：\(score)"
+//        finishLabel.horizontalAlignmentMode = .left
+//        finishLabel.position = CGPoint(x: 512, y: 384)
+//        finishLabel.zPosition = 2
+//        addChild(finishLabel)
+        
+        let ac = UIAlertController(title: "Total Score: \(score)", message: "Total socre is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] ac in
+            // restart game
+            score = 0
+            nextLevel = 1
+            
+            player.removeFromParent()
+            self.removeAllChildren()
+            self.loadLevel()
+            self.createPlayer()
+        }))
+        
+        // Present the alert on the root view controller
+        if let viewController = self.view?.window?.rootViewController {
+            viewController.present(ac, animated: true, completion: nil)
         }
     }
 }
